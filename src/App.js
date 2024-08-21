38623d3a67c494be8673ee28d770a3b9cf29b43e
ravi <ravisingh19870903@gmail.com>
2024-08-21 18:27:59 +0530

    Removed unnecessary components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import React from "react";
-import Employee from "./components/Employee";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import EmployeeTable from "./components/EmployeeTable";
+import Pagination from "./components/Pagination";
 const App = () => {
+  const [empData, setEmpData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPge, SetPostsPerPage] = useState(10);
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(
+        "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+      );
+      const json = await res.json();
+      setEmpData([...json]);
+      setLoading(false);
+    } catch (error) {
+      alert(error);
+    }
+  };
+  const handlePagination = (operation) => {
+    if (operation === "+") {
+      setCurrentPage((prev) => prev + 1);
+    } else if (operation === "-") {
+      setCurrentPage((prev) => prev - 1);
+    }
+  };
+
+  const indexOfLastPost = currentPage * postsPerPge;
+  const indexOfFirstPost = indexOfLastPost - postsPerPge;
+  const currentPosts = empData.slice(indexOfFirstPost, indexOfLastPost);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className='container'>
-      <Employee />
+      <h3>Employee Data Table</h3>
+      <EmployeeTable data={currentPosts} loading={loading} />
+      <Pagination
+        length={empData.length}
+        postPerPage={postsPerPge}
+        handlePagination={handlePagination}
+        currentPage={currentPage}
+      />
     </div>
   );
 };
